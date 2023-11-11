37dd25b24c6e5c95d0ec340cfdbce6fa0f816e35
Dylan E. Richards <dylanerichards@gmail.com>
2023-11-11 14:31:49 -0500

    Create Clients model and table

---
+++ db/migrate/20231111193049_create_buildings.rb
@@ -0,0 +1,13 @@
+class CreateBuildings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :buildings do |t|
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.references :client, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_11_191923) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_11_193049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buildings", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.bigint "client_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["client_id"], name: "index_buildings_on_client_id"
+  end
 
   create_table "clients", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_11_191923) do
     t.index ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "buildings", "clients"
 end
