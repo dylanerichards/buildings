e52849fd203f1b180776bbabfd71e37c2f6ef6ab
Dylan E. Richards <dylanerichards@gmail.com>
2023-11-23 16:31:24 -0500

    Add rubocop

--- Gemfile
+++ Gemfile
@@ -84,3 +84,5 @@ gem "rspec-rails", "~> 6.0"
 gem "active_model_serializers", "~> 0.10.14"
 
 gem "pagy", "~> 6.2"
+
+gem "rubocop", "~> 1.57"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,58 +8,57 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
-clients = ["dylan", "tony", "renee", "regina", "brandon"]
+clients = %w[dylan tony renee regina brandon]
 
 clients.each do |client|
-   c = Client.create(
+  c = Client.create(
     name: client,
     email: "#{client}@#{client}.com",
-    password: "password", 
-    password_confirmation: "password"
-  ) 
+    password: 'password',
+    password_confirmation: 'password'
+  )
 
   building = Building.create(
     address: "1 #{client} Way",
-    city: "New York",
-    state: "New York",
-    zip: "11221",
+    city: 'New York',
+    state: 'New York',
+    zip: '11221',
     client_id: c.id
   )
 
   CustomField.create(
     building_id: building.id,
-    field_name: "pets",
-    value: "allowed",
-    field_type: "freeform"
+    field_name: 'pets',
+    value: 'allowed',
+    field_type: 'freeform'
   )
 
   CustomField.create(
     building_id: building.id,
-    field_name: "bathrooms",
-    value: "1.5",
-    field_type: "number"
+    field_name: 'bathrooms',
+    value: '1.5',
+    field_type: 'number'
   )
 
   building = Building.create(
     address: "2 #{client} Way",
-    city: "New York",
-    state: "New York",
-    zip: "11221",
+    city: 'New York',
+    state: 'New York',
+    zip: '11221',
     client_id: c.id
   )
 
   CustomField.create(
     building_id: building.id,
-    field_name: "luxury",
-    value: "yes",
-    field_type: "freeform"
+    field_name: 'luxury',
+    value: 'yes',
+    field_type: 'freeform'
   )
 
   CustomField.create(
     building_id: building.id,
-    field_name: "pre-war",
-    value: "yes",
-    field_type: "freeform"
+    field_name: 'pre-war',
+    value: 'yes',
+    field_type: 'freeform'
   )
 end
